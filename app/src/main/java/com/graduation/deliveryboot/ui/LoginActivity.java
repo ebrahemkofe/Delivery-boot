7cbbd75671041d7c8a6cbb2af78a3b1b28b42cc7
ebrahemkofe <ebrahem.yousef@gmail.com>
2021-11-30 16:10:09 +0200

    Add Login Activity

---
+++ app/src/main/java/com/graduation/deliveryboot/ui/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.graduation.deliveryboot.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.graduation.deliveryboot.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
