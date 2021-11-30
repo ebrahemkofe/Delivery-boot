212645086e257e43c2436d8bf43c7b3ecbfa14b3
ebrahemkofe <ebrahem.yousef@gmail.com>
2021-11-30 21:04:25 +0200

    Edit home screen.

--- app/src/main/java/com/graduation/deliveryboot/Fragment/HomeFragment.java
+++ app/src/main/java/com/graduation/deliveryboot/Fragment/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.graduation.deliveryboot;
+package com.graduation.deliveryboot.Fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.graduation.deliveryboot.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeScreen#newInstance} factory method to
+ * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeScreen extends Fragment {
+public class HomeFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class HomeScreen extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public HomeScreen() {
+    public HomeFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +39,8 @@ public class HomeScreen extends Fragment {
      * @return A new instance of fragment HomeScreen.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeScreen newInstance(String param1, String param2) {
-        HomeScreen fragment = new HomeScreen();
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class HomeScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_last_orders_screen, container, false);
+        return inflater.inflate(R.layout.fragment_Home, container, false);
     }
 }
