4be7de3e108fd2d979e79d5ca1c66107646ef599
ayamohamed18 <71231266+ayamohamed18@users.noreply.github.com>
2021-12-01 03:47:13 +0200

    projectUpdated

---
+++ app/src/main/java/com/graduation/deliveryboot/ui/LastOrders.java
@@ -0,0 +1,13 @@
+package com.graduation.deliveryboot.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.graduation.deliveryboot.R;
+
+public class LastOrders extends AppCompatActivity {
+    protected void  onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_last_orders_screen);}
+}
