5b302edcd7b78a6e6d61ccfa68fa0ab0894cb3cb
ebrahemkofe <ebrahem.yousef@gmail.com>
2022-03-14 02:41:44 +0200

    Edit Custom Dialog Files. Edit ReceiveOrder Activity. Delete unUsed Drawables. Add Share and Copy Icons.

--- app/src/main/java/com/graduation/deliveryboot/Helper/CustomDialog.java
+++ app/src/main/java/com/graduation/deliveryboot/Helper/CustomDialog.java
@@ -1,16 +1,23 @@
 package com.graduation.deliveryboot.Helper;
 
+import static android.content.Context.CLIPBOARD_SERVICE;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.graduation.deliveryboot.Adapters.DialogListViewAdapter;
 import com.graduation.deliveryboot.R;
@@ -23,9 +30,11 @@ public class CustomDialog extends Dialog{
 
     public Context c;
     public Dialog d;
+
     ListView listView;
-    TextView textView;
-    Button YesButton , NoButton;
+    TextView textView , CodeText;
+    Button YesButton , NoButton , Done;
+    ImageView Copy , Share;
     String text;
     DialogListViewAdapter adapter;
     List<String> Device = new ArrayList<>();
@@ -94,6 +103,45 @@ public class CustomDialog extends Dialog{
                 @Override
                 public void onClick(View view) {
                     CustomDialog.this.cancel();
+                }
+            });
+
+        }
+
+        else if (Case == 2){
+            setContentView(R.layout.code_dialog);
+            Done = findViewById(R.id.DoneDialogButton);
+            Copy = findViewById(R.id.CopyIcon);
+            Share = findViewById(R.id.ShareIcon);
+            CodeText = findViewById(R.id.CodeText);
+
+            Done.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    CustomDialog.this.cancel();
+                }
+            });
+
+            Copy.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ClipboardManager clipboard = (android.content.ClipboardManager) c.getSystemService(Context.CLIPBOARD_SERVICE);
+                    ClipData clip = android.content.ClipData.newPlainText("Copied", CodeText.getText());
+
+                    clipboard.setPrimaryClip(clip);
+
+                    Toast.makeText(c, "Copied", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            Share.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent sendIntent = new Intent();
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, "This is Receiving Code!  " + CodeText.getText());
+                    sendIntent.setType("text/plain");
+                    c.startActivity(sendIntent);
                 }
             });
         }
