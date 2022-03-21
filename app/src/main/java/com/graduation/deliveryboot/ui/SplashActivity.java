7f9ac5a7caca7c701af4cb487875888f258017d8
ebrahemkofe <ebrahem.yousef@gmail.com>
2022-03-21 03:53:44 +0200

    Add SplashActivity

---
+++ app/src/main/java/com/graduation/deliveryboot/ui/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.graduation.deliveryboot.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.graduation.deliveryboot.R;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
